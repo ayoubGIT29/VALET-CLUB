8704edad6c057e6933773bef7066ee21d578fb4c
Ayoub <ayoubmounadi50@gmail.com>
2024-07-29 15:47:23 +0100

    Initial commit

---
+++ src/components/Layouts/Sidebar.tsx
@@ -0,0 +1,199 @@
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
+import { toggleSidebar } from '../../store/themeConfigSlice';
+import { IRootState } from '../../store';
+import { useState, useEffect } from 'react';
+import IconCaretsDown from '../Icon/IconCaretsDown';
+import IconMenuDashboard from '../Icon/Menu/IconMenuDashboard';
+import IconMenuChat from '../Icon/Menu/IconMenuChat';
+import IconMenuMailbox from '../Icon/Menu/IconMenuMailbox';
+import IconMenuTodo from '../Icon/Menu/IconMenuTodo';
+import IconMenuNotes from '../Icon/Menu/IconMenuNotes';
+import IconMenuScrumboard from '../Icon/Menu/IconMenuScrumboard';
+import IconMenuContacts from '../Icon/Menu/IconMenuContacts';
+import IconMenuInvoice from '../Icon/Menu/IconMenuInvoice';
+import IconMenuCalendar from '../Icon/Menu/IconMenuCalendar';
+import IconMenuComponents from '../Icon/Menu/IconMenuComponents';
+import IconMenuElements from '../Icon/Menu/IconMenuElements';
+import IconMenuCharts from '../Icon/Menu/IconMenuCharts';
+import IconMenuWidgets from '../Icon/Menu/IconMenuWidgets';
+import IconMenuFontIcons from '../Icon/Menu/IconMenuFontIcons';
+import IconMenuDragAndDrop from '../Icon/Menu/IconMenuDragAndDrop';
+import IconMenuTables from '../Icon/Menu/IconMenuTables';
+
+import IconMinus from '../Icon/IconMinus';
+
+const Sidebar = () => {
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const semidark = useSelector((state: IRootState) => state.themeConfig.semidark);
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        if (window.innerWidth < 1024 && themeConfig.sidebar) {
+            dispatch(toggleSidebar());
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location]);
+
+    return (
+        <div className={semidark ? 'dark' : ''}>
+            <nav
+                className={`sidebar fixed min-h-screen h-full top-0 bottom-0 w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] z-50 transition-all duration-300 ${semidark ? 'text-white-dark' : ''}`}
+            >
+                <div className="bg-white dark:bg-black h-full">
+                    <div className="flex justify-between items-center px-4 py-3">
+                        <NavLink to="/" className="main-logo flex items-center shrink-0">
+                            <img className="w-8 ml-[5px] flex-none" src="/assets/images/logo.svg" alt="logo" />
+                            <span className="text-2xl ltr:ml-1.5 rtl:mr-1.5 font-semibold align-middle lg:inline dark:text-white-light">{t('VALET CLUB')}</span>
+                        </NavLink>
+
+                        <button
+                            type="button"
+                            className="collapse-icon w-8 h-8 rounded-full flex items-center hover:bg-gray-500/10 dark:hover:bg-dark-light/10 dark:text-white-light transition duration-300 rtl:rotate-180"
+                            onClick={() => dispatch(toggleSidebar())}
+                        >
+                            <IconCaretsDown className="m-auto rotate-90" />
+                        </button>
+                    </div>
+                    <PerfectScrollbar className="h-[calc(100vh-80px)] relative">
+                        <ul className="relative font-semibold space-y-0.5 p-4 py-0">
+                            <li className="nav-item">
+                                <NavLink to="/" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuDashboard className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Dashboard')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Operations" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuChat className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Opérations')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Valets" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuMailbox className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Valets')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Clients" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuTodo className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Clients')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Vehicules" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuNotes className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Véhicules')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Endroits" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuScrumboard className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Endroits préférés')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Services" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuContacts className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('services')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/invoice/Partenaires" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuInvoice className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Partenaires')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/Hotspots" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuCalendar className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Hotspots')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/components/Responsables" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuComponents className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Responsables')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink to="/elements/Paiements" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuElements className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Paiements')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+            
+                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
+                                <IconMinus className="w-4 h-5 flex-none hidden" />
+                                <span>{t('Tracking')}</span>
+                            </h2>
+                        
+                            <li className="nav-item">
+                                <NavLink to="/Géocolisation" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuDragAndDrop className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Géocolisation')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+                            
+                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
+                                <IconMinus className="w-4 h-5 flex-none hidden" />
+                                <span>{t('Utilisateurs')}</span>
+                            </h2>
+
+                            <li className="nav-item">
+                                <NavLink to="/Administrateurs" className="group">
+                                    <div className="flex items-center">
+                                        <IconMenuTables className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">{t('Administrateurs')}</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                        </ul>
+                    </PerfectScrollbar>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
